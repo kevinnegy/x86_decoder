aef6513e626dfa609b4a76c78594ddb70900b820
Kevin Negy <kevinnegy@gmail.com>
2020-06-20 23:45:53 +0000

    Added char pointers to struct x86_instr to store bytes of each instruction segment. E.g. To get REX byte, just access inst->rex

--- instruction.c
+++ instruction.c
@@ -4,30 +4,58 @@
 #include "instruction.h"
 
 void build_x86_string(struct x86_instr * inst);
-
 void check_sib(struct x86_instr * inst);
 void check_displacement(struct x86_instr * inst);
 void check_immediate(struct x86_instr * inst);
 
-
-// Copies user-provided bytecode into new x86 struct
-// TODO maybe change this to something else. Is a function necessary?
 struct x86_instr * create_x86_instr(char * bytes){ 
+    if(bytes == NULL){
+         printf("%s: input bytes == NULL\n", __func__); exit(-1); 
+    }
 
     struct x86_instr * inst = (struct x86_instr *) malloc(sizeof(struct x86_instr));
+    if(inst == NULL){
+        printf("%s: %s\n", __func__, "inst malloc failed");
+    }
     
-    inst->prefix_flag = 0; // If checkprefix finds a prefix, this will be set to 1
-    inst->rex_ptr = 0; 
-    inst->opcode_ptr = 0; 
-    inst->modrm_ptr = 0; 
-    inst->sib_ptr = 0; 
-    inst->immediate_ptr = 0; 
-    inst->displacement_ptr = 0; 
+    // Instantiate all index ptrs to -1
+    inst->prefix_ptr = inst->rex_ptr = inst->opcode_ptr = inst->modrm_ptr =  
+        inst->sib_ptr = inst->immediate_ptr = inst->displacement_ptr = -1; 
 
+    // Store original byte code
     inst->byte_code = (unsigned char *) malloc(sizeof(unsigned char) * MAX_INSTR_LEN); 
+    if(inst->byte_code == NULL){
+        printf("%s: %s\n", __func__, "inst->byte_code malloc failed");
+    }
     memcpy(inst->byte_code, bytes, MAX_INSTR_LEN); 
 
+    inst->rex = inst->modrm = inst->sib = 0x0;
+
+    // Malloc max array lens for each instruction segment
+    inst->prefixes = (unsigned char *) malloc(sizeof(unsigned char) * 4); // 4 groups, 1 each max
+    if(inst->prefixes == NULL){
+        printf("%s: %s\n", __func__, "inst->prefixes malloc failed");
+    }
+    inst->opcode = (unsigned char *) malloc(sizeof(unsigned char) * 3);  
+    if(inst->opcode == NULL){
+        printf("%s: %s\n", __func__, "inst->opcode malloc failed");
+    }
+    inst->displacement = (unsigned char *) malloc(sizeof(unsigned char) * 4);  
+    if(inst->displacement == NULL){
+        printf("%s: %s\n", __func__, "inst->displacement malloc failed");
+    }
+    inst->immediate = (unsigned char *) malloc(sizeof(unsigned char) * 4);  
+    if(inst->immediate == NULL){
+        printf("%s: %s\n", __func__, "inst->immediate malloc failed");
+    }
+
+    // Start off array lens at 0
+    inst->prefix_len = inst->opcode_len = inst->displacement_len = inst->immediate_len = 0; 
+    
     inst->x86_string = (char *) malloc(sizeof(char) * 50);
+    if(inst->x86_string == NULL){
+        printf("%s: %s\n", __func__, "inst->x86_string malloc failed");
+    }
 
     return inst;
 
@@ -35,6 +63,10 @@ struct x86_instr * create_x86_instr(char * bytes){
 
 void delete_x86_instr(struct x86_instr * inst){
     free(inst->byte_code);
+    free(inst->prefixes);
+    free(inst->opcode);
+    free(inst->displacement);
+    free(inst->immediate);
     free(inst->x86_string);
     free(inst);
 
@@ -44,11 +76,3 @@ void delete_x86_instr(struct x86_instr * inst){
 char * get_string(struct x86_instr * inst){
     return inst->x86_string;
 }
-
-
-/** Displacement/Immediate **/
-// Address displacement - 1, 2, or 4 bytes or none
-// Immediate - 1,2,4, or none
-
-
-
