52893abdcc2282822236c73a436f252439f9edb7
Kevin Negy <kevinnegy@gmail.com>
2020-06-28 22:30:43 +0000

    Instruction.h has an attempt at using unions to combine instructions with different opcode length. Problem is union takes the size of the largest member so can't be used as overlay struct pointer.

--- instruction.h
+++ instruction.h
@@ -9,63 +9,39 @@
 
 int DEFAULT_BIT_MODE;
 
-/*** One byte opcode ***/
-struct rex_one_op_modrm{
+struct rex_modrm{
     u_int8_t rex_prefix: 4;
     u_int8_t rex_w: 1;
     u_int8_t rex_r: 1;
     u_int8_t rex_x: 1;
     u_int8_t rex_b: 1;
-    u_int8_t opcode: 8;
-    u_int8_t modrm: 2;
-    u_int8_t reg: 3;
-    u_int8_t rm: 3;
-};
-
-struct one_op_modrm{
-    u_int8_t opcode: 8;
-    u_int8_t modrm: 2;
-    u_int8_t reg: 3;
-    u_int8_t rm: 3;
-};
-
-struct one_op_disp_imm{
-    u_int8_t opcode: 8;
     union{
-        unsigned char disp8[1];
-        unsigned char disp16[2];
-        unsigned char disp32[4];
-        unsigned char imm8[1];
-        unsigned char imm16[2];
-        unsigned char imm32[4];
+        unsigned char single_opcode[1];
+        unsigned char double_opcode[2]; 
+        unsigned char triple_opcode[3]; 
     };
-};
-
-/*** Two byte opcode ***/
-struct rex_two_op_modrm{
-    u_int8_t rex_prefix: 4;
-    u_int8_t rex_w: 1;
-    u_int8_t rex_r: 1;
-    u_int8_t rex_x: 1;
-    u_int8_t rex_b: 1;
-    u_int8_t opcode_1: 8;
-    u_int8_t opcode_2: 8;
     u_int8_t modrm: 2;
     u_int8_t reg: 3;
     u_int8_t rm: 3;
 };
 
-struct two_op_modrm{
-    u_int8_t opcode_1: 8;
-    u_int8_t opcode_2: 8;
+struct modrm{
+    union{
+        unsigned char single_opcode[1];
+        unsigned char double_opcode[2]; 
+        unsigned char triple_opcode[3]; 
+    };
     u_int8_t modrm: 2;
     u_int8_t reg: 3;
     u_int8_t rm: 3;
 };
 
-struct two_op_disp_imm{
-    u_int8_t opcode_1: 8;
-    u_int8_t opcode_2: 8;
+struct disp_imm{
+    union{
+        unsigned char single_opcode[1];
+        unsigned char double_opcode[2]; 
+        unsigned char triple_opcode[3]; 
+    };
     union{
         unsigned char disp8[1];
         unsigned char disp16[2];
