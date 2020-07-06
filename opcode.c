53a82ad752d567ddd751725eb7c05886c612cb88
Kevin Negy <kevinnegy@gmail.com>
2020-07-06 07:49:56 +0000

    AND, CMP, JCC, NEG, NOP, Shift instructions

--- opcode.c
+++ opcode.c
@@ -47,20 +47,8 @@ void check_second_opcode(unsigned char * inst, int operand_override, int address
     int operand_size = find_operand_size(operand_override, rex);
     int address_size = find_address_size(operand_override);
     switch(opcode){
-        case OP_JZJE_84: 
-        {
-            // TODO handle
-            int64_t disp = get_displacement(&inst[1], DEFAULT_BIT_MODE, 0);
-
-            if(DEFAULT_BIT_MODE == 16)
-                disp = disp + 4; 
-            else
-                disp = disp + 6; 
-
-            printf("disp 0x%lx\n", disp);
-
-            return;
-        }
+        case OP_NOP_1F: 
+            return; 
 
         case OP_POP_A1:
         case OP_POP_A9:
@@ -70,12 +58,39 @@ void check_second_opcode(unsigned char * inst, int operand_override, int address
             printf("memory access [rsp]\n"); 
             return;
 
+        case OP_CMOV_44:
+            check_modrm_rm(&inst[1], operand_size, address_size, rex);
+            check_modrm_reg(&inst[1], operand_size, address_size, rex);
+            return;
+
         case OP_RDTSC:
             return;
 
-        default:
-            assert(0);
     }
+    if((opcode & 0x80) == 0x80){
+        switch(opcode & 0xf){
+            case 0: // JO (0F = 1)
+            case 1: // JNO (0F = 0)
+            case 2: // JB/JC/JNAE (CF = 1)
+            case 3: // JAE/JNB/JNC (CF = 0)
+            case 4: // JE/JZ (ZF = 1)
+            case 5: // JNZ (ZF = 0) 
+            case 6: // JBE/JNA (CF = 1 or ZF = 1)
+            case 7: // JA/JNBE (if CF = 0 and ZF = 0)
+            case 8: // JS (SF = 1) 
+            case 9: // JNS (SF = 0) 
+            case 0xa: // JP/JPE (PF = 1) 
+            case 0xb: // JNP/JPO (PF = 0)
+            case 0xc: // JL (SF != OF)
+            case 0xd: // JGE/JNL (SF = OF)
+            case 0xf: // JG/JNLE (ZF = 0 and SF = OF)
+            case 0xe: // JGE/JNG (ZF = 1 or SF != OF)
+                printf("Conditional displacement! 0x%lx\n", get_displacement(&inst[1], 32, 6)); // 6bytes/inst
+                return;
+        }
+    }
+
+    assert(0);
     return;
 }
 
@@ -93,6 +108,9 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
     int address_size = find_address_size(operand_override);
 
     switch(opcode){
+        case OP_NOP_90:
+            return;
+
         case OP_MOV_A0: case OP_MOV_A1: case OP_MOV_A2: case OP_MOV_A3:
         case OP_MOV_8C:
         case OP_MOV_8E:
@@ -114,6 +132,8 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
 
         // 8 bit operand case - rm (dest), reg
         case OP_ADD_00:
+        case OP_AND_20:
+        case OP_CMP_38:
         case OP_MOV_88:
         case OP_OR_08:
         case OP_SUB_28:
@@ -123,6 +143,8 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
 
         // 8 bit operand case - reg (dest), rm
         case OP_ADD_02:
+        case OP_AND_22:
+        case OP_CMP_3A:
         case OP_MOV_8A:
         case OP_OR_0A:
         case OP_SUB_2A:
@@ -132,6 +154,8 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
 
         // Normal - rm (dest), reg 
         case OP_ADD_01:
+        case OP_AND_21:
+        case OP_CMP_39:
         case OP_MOV_89:
         case OP_OR_09:
         case OP_SUB_29:
@@ -141,6 +165,8 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
 
         // Normal - reg (dest), rm 
         case OP_ADD_03:
+        case OP_AND_23:
+        case OP_CMP_3B:
         case OP_LEA:
         case OP_MOV_8B:
         case OP_OR_0B:
@@ -152,6 +178,8 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
 
         // ax,eax,rax imm16,32
         case OP_ADD_05:
+        case OP_AND_25:
+        case OP_CMP_3D:
         case OP_OR_0D:
         case OP_SUB_2D:
         case OP_TEST_A9:
@@ -164,6 +192,8 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
         
         // AL imm8
         case OP_ADD_04:
+        case OP_AND_24:
+        case OP_CMP_3C:
         case OP_OR_0C:
         case OP_SUB_2C:
         case OP_TEST_A8:
@@ -186,7 +216,9 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
             switch((inst[1] & 0x38) >> 3){
                 case 0: // Add
                 case 1: // Or
+                case 4: // AND
                 case 5: // Sub
+                case 7: // CMP
                     check_modrm_rm(&inst[1], 8, address_size, rex);
                     get_immediate(&inst[2], 8);
                     return;
@@ -200,7 +232,9 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
             switch((inst[1] & 0x38) >> 3){
                 case 0: // Add
                 case 1: // Or
+                case 4: // AND
                 case 5: // Sub
+                case 7: // CMP
                     check_modrm_rm(&inst[1], operand_size, address_size, rex);
                     if(operand_size == 64)
                         get_immediate(&inst[2], 32);
@@ -224,7 +258,9 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
             switch((inst[1] & 0x38) >> 3){
                 case 0: // Add
                 case 1: // Or
+                case 4: // AND
                 case 5: // Sub
+                case 7: // CMP
                     check_modrm_rm(&inst[1], operand_size, address_size, rex);
                     get_immediate(&inst[2], 8);
                     return;
@@ -246,12 +282,91 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
             }
             return;
 
+        case 0xC0:
+            switch((inst[1] & 0x38) >> 3){
+                case 4: // SAL/SHL rm8 imm8 
+                case 5: // SHR
+                case 7: // SAR
+                    check_modrm_rm(&inst[1], 8, address_size, rex);
+                    get_immediate(&inst[2], 8);
+                    return;
+                default:
+                    assert(0);
+            }
+            return;
+
+        case 0xC1:
+            switch((inst[1] & 0x38) >> 3){
+                case 4: // SAL/SHL imm8 
+                case 5: // SHR
+                case 7: // SAR
+                    check_modrm_rm(&inst[1], operand_size, address_size, rex);
+                    get_immediate(&inst[2], 8);
+                    return;
+                default:
+                    assert(0);
+            }
+            return;
+
+        case 0xD0:
+            switch((inst[1] & 0x38) >> 3){
+                case 4: // SAL/SHL 1 time
+                case 5: // SHR
+                case 7: // SAR 1 time
+                    check_modrm_rm(&inst[1], 8, address_size, rex);
+                    return;
+                default:
+                    assert(0);
+            }
+            return;
+
+        case 0xD1:
+            switch((inst[1] & 0x38) >> 3){
+                case 4: // SAL/SHL 1 time
+                case 5: // SHR
+                case 7: // SAR
+                    check_modrm_rm(&inst[1], operand_size, address_size, rex);
+                    return;
+                default:
+                    assert(0);
+            }
+            return;
+
+
+        case 0xD2:
+            switch((inst[1] & 0x38) >> 3){
+                case 4: // SAL/SHL CL number of times
+                case 5: // SHR
+                case 7: // SAR
+                    check_modrm_rm(&inst[1], 8, address_size, rex);
+                    return;
+                default:
+                    assert(0);
+            }
+            return;
+
+         case 0xD3:
+            switch((inst[1] & 0x38) >> 3){
+                case 4: // SAL/SHL CL number of times
+                case 5: // SHR
+                case 7: // SAR
+                    check_modrm_rm(&inst[1], operand_size, address_size, rex);
+                    return;
+                default:
+                    assert(0);
+            }
+            return;
+            
+
         case 0xF6:
             switch((inst[1] & 0x38) >> 3){
                 case 0: // Test 
                     // Test does no memory writing, skip modrm_rm call 
                     //check_modrm_rm(&inst[1], 8, address_size, rex);
                     get_immediate(&inst[2], 8);
+                    return;
+                case 3: // Neg rm
+                    check_modrm_rm(&inst[1], 8, address_size, rex);
                     return;
                 default:
                     assert(0);
@@ -267,6 +382,10 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
                         get_immediate(&inst[2], 32);
                     else
                         get_immediate(&inst[2], operand_size);
+                    return;
+                case 3:
+                    check_modrm_rm(&inst[1], operand_size, address_size, rex);
+                    return;
                 default:
                     assert(0);
             }
@@ -314,10 +433,6 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
             get_immediate(&inst[1], operand_size);
             printf("memory access [rsp]\n"); 
             return;
-
-
-        case OP_SHL:
-            return;
     }
 
     // Opcodes whose last 3 bits are for one register
@@ -329,7 +444,7 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
 
         case OP_MOV_B8:
             get_register(opcode & 0x7, operand_size, rex); 
-            get_immediate(&inst[1], operand_size);
+            get_immediate(&inst[1], 8);
             return;
 
         case OP_POP_58:
@@ -339,9 +454,38 @@ void check_opcode(unsigned char * inst, int operand_override, int address_overri
             get_register(opcode & 0x7, operand_size, 0);
             printf("memory access [rsp]\n"); 
             return;
-        default:
-            assert(0);
-            return;
     }
+
+    // JCC instructions
+    if((opcode & 0x70) == 0x70){
+        switch(opcode & 0xf){
+            case 0: // JO (0F = 1)
+            case 1: // JNO (0F = 0)
+            case 2: // JB/JC/JNAE (CF = 1)
+            case 3: // JAE/JNB/JNC (CF = 0)
+            case 4: // JE/JZ (ZF = 1)
+            case 5: // JNZ (ZF = 0) 
+            case 6: // JBE/JNA (CF = 1 or ZF = 1)
+            case 7: // JA/JNBE (if CF = 0 and ZF = 0)
+            case 8: // JS (SF = 1) 
+            case 9: // JNS (SF = 0) 
+            case 0xa: // JP/JPE (PF = 1) 
+            case 0xb: // JNP/JPO (PF = 0)
+            case 0xc: // JL (SF != OF)
+            case 0xd: // JGE/JNL (SF = OF)
+            case 0xf: // JG/JNLE (ZF = 0 and SF = OF)
+            case 0xe: // JGE/JNG (ZF = 1 or SF != OF)
+                printf("Conditional displacement! 0x%lx\n", get_displacement(&inst[1], 8, 2)); // TODO check if instruction length matters here
+            return;
+        }
+    }
+
+    else if(opcode == OP_JCC_E3){
+        // TODO handle operand size defaulting to 64 bit without rex 
+        printf("Conditional displacement! 0x%lx\n", get_displacement(&inst[1], operand_size, 0)); // TODO check if instruction length matters here
+        return;
+    }
+
+    assert(0);
     return;
 }
--- opcode.h
+++ opcode.h
@@ -5,19 +5,62 @@
 // Can be 1,2, or 3 bytes. A fourth can be in ModRM
 enum one_byte_opcodes{
     // Add - done
-    OP_ADD_00 = 0x0, // 8 modrm - reg->rm
-    OP_ADD_01 = 0x1, // 16,32,64 modrm - reg->rm
-    OP_ADD_02 = 0x2, // 8 modrm - rm->reg
-    OP_ADD_03 = 0x3, // 16,32,64 modrm - rm->reg
     OP_ADD_04 = 0x4, // 8 AL imm8
     OP_ADD_05 = 0x5, // 16,32,64 ax,eax,rax imm16,32 
     OP_ADD_80 = 0x80, // 8 modrm imm8 - imm8->rm8
     OP_ADD_81 = 0x81, // 16,32,64 rm imm16,32 - imm8->rm
     OP_ADD_83 = 0x83, // 16,32,64 rm imm8 - imm8->rm
+    OP_ADD_00 = 0x0, // 8 modrm - reg->rm
+    OP_ADD_01 = 0x1, // 16,32,64 modrm - reg->rm
+    OP_ADD_02 = 0x2, // 8 modrm - rm->reg
+    OP_ADD_03 = 0x3, // 16,32,64 modrm - rm->reg
+
+    // AND - done
+    OP_AND_24 = 0x24, // 8 AL imm8
+    OP_AND_25 = 0x25, // 16,32,64 ax,eax,rax imm16,32 
+    OP_AND_80 = 0x80, // 8 modrm imm8 - imm8->rm8
+    OP_AND_81 = 0x81, // 16,32,64 rm imm16,32 - imm8->rm
+    OP_AND_83 = 0x83, // 16,32,64 rm imm8 - imm8->rm
+    OP_AND_20 = 0x20, // 8 modrm - reg->rm
+    OP_AND_21 = 0x21, // 16,32,64 modrm - reg->rm
+    OP_AND_22 = 0x22, // 8 modrm - rm->reg
+    OP_AND_23 = 0x23, // 16,32,64 modrm - rm->reg
+    
 
     // Call -done 
     OP_CALL_E8 = 0xe8, // disp 32-> 64 bit sign extend
     OP_CALL_FF = 0xff, // rm64 or jump to different location
+
+    // CMP - done
+    OP_CMP_3C = 0x3c, // 8 AL imm8
+    OP_CMP_3D = 0x3d, // 16,32,64 ax,eax,rax imm16,32 
+    OP_CMP_80 = 0x80, // 8 modrm imm8 - imm8->rm8
+    OP_CMP_81 = 0x81, // 16,32,64 rm imm16,32 - imm8->rm
+    OP_CMP_83 = 0x83, // 16,32,64 rm imm8 - imm8->rm
+    OP_CMP_38 = 0x38, // 8 modrm - reg->rm
+    OP_CMP_39 = 0x39, // 16,32,64 modrm - reg->rm
+    OP_CMP_3A = 0x3a, // 8 modrm - rm->reg
+    OP_CMP_3B = 0x3b, // 16,32,64 modrm - rm->reg
+
+    // JCC -done (see opcode.c for which flags are in the condition)
+    // TODO handle instructions that default to 64 bit operands in long mode like jcc
+    OP_JCC_70 = 0x70,
+    OP_JCC_71 = 0x71,
+    OP_JCC_72 = 0x72,
+    OP_JCC_73 = 0x73,
+    OP_JCC_74 = 0x74,
+    OP_JCC_75 = 0x75,
+    OP_JCC_76 = 0x76,
+    OP_JCC_77 = 0x77,
+    OP_JCC_78 = 0x78,
+    OP_JCC_79 = 0x79,
+    OP_JCC_7a = 0x7a,
+    OP_JCC_7b = 0x7b,
+    OP_JCC_7c = 0x7c,
+    OP_JCC_7d = 0x7d,
+    OP_JCC_7e = 0x7e,
+    OP_JCC_7f = 0x7f,
+    OP_JCC_E3 = 0xe3, // JECXZ or JRCXZ (ECX/RCX = 0)
 
     // Jmp -done
     OP_JMP_EB = 0xeb,
@@ -27,6 +70,7 @@ enum one_byte_opcodes{
     // Lea done
     OP_LEA = 0x8d, // load effective address
 
+    // Mov -done
     OP_MOV_88 = 0x88, // 8 bit regs/mm - reg->rm
     OP_MOV_89 = 0x89, // 16,32,64 regs/mm - reg->rm
     OP_MOV_8A = 0x8a, // 8 bit regs/mm - rm->reg
@@ -41,6 +85,13 @@ enum one_byte_opcodes{
     OP_MOV_B8 = 0xb8, // imm16,32,64 -> reg16,32,64 (get reg from opcode B8-BF)
     OP_MOV_C6 = 0xc6, // 8 bit imm to reg - use only r/m in modrm
     OP_MOV_C7 = 0xc7, // 16,32,64 rm imm16,32 
+
+    // Neg -done 
+    OP_NEG_F6 = 0xf6, // 8 bit rm 
+    OP_NEG_F7 = 0xf7, // 16,32,64 bit rm 
+    
+    // Nop -done
+    OP_NOP_90 = 0x90, // nop one byte
 
     // Or - done
     OP_OR_0C = 0x0c, // 8 AL imm8
@@ -63,6 +114,30 @@ enum one_byte_opcodes{
     OP_PUSH_68 = 0x68, // push imm16,32 
     OP_PUSH_50 = 0x50, // push is 50-57, last 3 bits are for register
 
+    // SHL - done (SHL and SAL are the same)
+    OP_SHL_C0 = 0xc0, // 8 bit rm shift imm8 number of times 
+    OP_SHL_C1 = 0xc1, // 16,32,64 bit rm shift imm8 # of times
+    OP_SHL_D0 = 0xd0, // 8 bit rm shift once left 
+    OP_SHL_D1 = 0xd1, // 16,32,64 rm shift once left 
+    OP_SHL_D2 = 0xd2, // 8 bit rm shift CL register number of times 
+    OP_SHL_D3 = 0xd3, // 16,32,64 bit rm shift CL register number of times 
+    
+    // SAR - done (sign bit)
+    OP_SAR_C0 = 0xc0, // 8 bit rm shift imm8 number of times 
+    OP_SAR_C1 = 0xc1, // 16,32,64 bit rm shift imm8 # of times
+    OP_SAR_D0 = 0xd0, // 8 bit rm shift right once 
+    OP_SAR_D1 = 0xd1, // 16,32,64 rm shift once 
+    OP_SAR_D2 = 0xd2, // 8 bit rm shift CL register number of times 
+    OP_SAR_D3 = 0xd3, // 16,32,64 bit rm shift CL register number of times 
+    
+    // SHR -done (unsigned) 
+    OP_SHR_C0 = 0xc0, // 8 bit rm shift imm8 number of times 
+    OP_SHR_C1 = 0xc1, // 16,32,64 bit rm shift imm8 # of times
+    OP_SHR_D0 = 0xd0, // 8 bit rm shift right once 
+    OP_SHR_D1 = 0xd1, // 16,32,64 rm shift once 
+    OP_SHR_D2 = 0xd2, // 8 bit rm shift CL register number of times 
+    OP_SHR_D3 = 0xd3, // 16,32,64 bit rm shift CL register number of times 
+
     // Sub -done
     OP_SUB_2C = 0x2c,  // 8 AL imm8
     OP_SUB_2D = 0x2d,  // 16,32,64 ax,eax,rax imm16,32 
@@ -74,7 +149,6 @@ enum one_byte_opcodes{
     OP_SUB_2A = 0x2a, // 8bit regs/mm - rm->reg  
     OP_SUB_2B = 0x2b, // normal rm->reg 
 
-
     // Test - done, no memory writing, just ANDs operands and sets SF PF ZF flags
     OP_TEST_A8 = 0xa8, // 8 AL imm8 
     OP_TEST_A9 = 0xa9, // 16,32,64 ax,eax,rax imm16,32 
@@ -82,15 +156,14 @@ enum one_byte_opcodes{
     OP_TEST_F7 = 0xf7, // 16,32,64 rm imm16,32 
     OP_TEST_84 = 0x84, // 8bit regs/mm - reg->rm 
     OP_TEST_85 = 0x85, // normal reg->rm 
-
-    // TODO handle
-    OP_SHL = 0xc1, // shift logical left always imm8
 };
 
 enum two_byte_opcodes{
-    // TODO handle
-    OP_JZJE_84 = 0x84, // jump if equal or zero (meaning if zf flag = 1)
+    OP_CMOV_44 = 0x44, // CMOVZ
 
+    // Done
+    OP_JCC_80 = 0x80, // 64 bit mode JCC instructions are 80-8F (see one byte JCC instructions for description)
+    OP_NOP_1F = 0x1f, // NOP multi byte (rm32,64)
     OP_POP_A1 = 0xa1, // pop FS register
     OP_POP_A9 = 0xa9, // pop GS register
     OP_PUSH_A0 = 0xa0, // push FS register
